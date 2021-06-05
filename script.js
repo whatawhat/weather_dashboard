b540e7cc8ada28259af1db82181899a4c4210a62
Stephanie <whatawhat@hotmail.com>
2021-06-05 17:48:31 -0400

    trying to add 5 day forecast; added labels for temperature, humidity and wind speed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const startButton = $("#searchButton")
-console.log("This is the button", startButton)
+//console.log("This is the button", startButton)
 startButton.click(function() {
     const searchInput = $("#searchValue").val();
     var apiKey = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&units=imperial&appid=57b85d9f6f550dd234e00f49ef09169c`
-    console.log("This is the input", searchInput)
+    //console.log("This is the input", searchInput)
     $.ajax({
         url:apiKey,
         method: "GET",
@@ -17,16 +17,16 @@ startButton.click(function() {
             temp.text(data.main.temp)
             humid.text(data.main.humidity)
             wind.text(data.wind.speed)
-            console.log(data)
+            //console.log(data)
             var oneCallApiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=57b85d9f6f550dd234e00f49ef09169c`
-            console.log(data.coord.lat)
-            console.log(data.coord.lon)
+            //console.log(data.coord.lat)
+            //console.log(data.coord.lon)
             $.ajax({
                 url:oneCallApiKey,
                 method: "GET",
                 success: function(data){
-                    console.log("This is the One Call API Key!", data)
-                    console.log("This is the UV Index", data.current.uvi)
+                    //console.log("This is the One Call API Key!", data)
+                    //console.log("This is the UV Index", data.current.uvi)
                     var uvElement = $("#uvInd")
                     if (data.current.uvi <= 2) {
                         uvElement.css("background-color", "green")
@@ -38,6 +38,10 @@ startButton.click(function() {
                         uvElement.css("background-color", "red")}
                     uvElement.text(data.current.uvi)
                     //5 day forecast
+                    var fiveDay = $("#fivedayforecast")
+                    fiveDay.text(data.daily.temp)
+
+
                 }
             })
 
