39c5cc4911561d1167cd2f3527c33dce8f39d64e
Stephanie <whatawhat@hotmail.com>
2021-06-10 13:27:58 -0400

    made cities clickable; hid labels until search is called

--- script.js
+++ script.js
@@ -5,102 +5,120 @@ const searchHistory = localStorage.getItem('search');
 const recentSearches = searchHistory ? JSON.parse(searchHistory) : [];
 recentSearch();
 
+function weatherResults(searchInput) {
+    var apiKey = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&units=imperial&appid=57b85d9f6f550dd234e00f49ef09169c`;
+    //console.log("This is the input", searchInput)
+    $.ajax({
+      url: apiKey,
+      method: "GET",
+      success: function (data) {
+          $("#currentWeather").show();
+        var cityTitle = $("#cityName");
+        var temp = $("#temp");
+        var humid = $("#humidity");
+        var wind = $("#windSpeed");
+        var dateString = moment.unix(data.dt).format("MM/DD/YYYY");
+        cityTitle.html(
+          `${data.name} ${dateString} <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>`
+        );
+        temp.text(data.main.temp);
+        humid.text(data.main.humidity);
+        wind.text(data.wind.speed);
+        //console.log(data)
+        var oneCallApiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=57b85d9f6f550dd234e00f49ef09169c`;
+        //console.log(data.coord.lat)
+        //console.log(data.coord.lon)
+        $.ajax({
+          url: oneCallApiKey,
+          method: "GET",
+          success: function (data) {
+            console.log("This is the One Call API Key!", data);
+            console.log("This is the UV Index", data.current.uvi);
+            var uvElement = $("#uvInd");
+            if (data.current.uvi <= 2) {
+              uvElement.css("background-color", "green");
+            } else if (data.current.uvi >= 3 && data.current.uvi <= 5) {
+              uvElement.css("background-color", "yellow");
+            } else if (data.current.uvi >= 6 && data.current.uvi <= 7) {
+              uvElement.css("background-color", "orange");
+            } else {
+              uvElement.css("background-color", "red");
+            }
+            uvElement.text(data.current.uvi);
+            //5 day forecast
+            var fiveDay = $("#fivedayforecast");
+            fiveDay.empty();
+            //fiveDay.text(data.daily[1].humidity)
+            //console.log("this is the daily humidity for day 1", data.daily[1].humidity)
+  
+            for (var i = 1; i < 6; i++) {
+              //fiveDay.text(data.daily[i].humidity)
+              console.log(data.daily[i]);
+              var divElement = $("<div class=fivedaybox></div>");
+              var dateElement = $("<p></p>");
+              var datefive = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+              dateElement.text(datefive);
+              var iconElement = $(
+                `<img src="http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"/>`
+              );
+              var tempElement = $("<p></p>");
+              var tempfive = data.daily[i].temp.day;
+              tempElement.text(tempfive);
+              var humidityElement = $("<p></p>");
+              humidityElement.text(data.daily[i].humidity);
+              divElement.append(
+                dateElement,
+                iconElement,
+                tempElement,
+                humidityElement
+              );
+              fiveDay.append(divElement);
+  
+              //tempterature, humidity, date, icon
+            }
+          },
+        });
+      },
+    });
+}
+
 startButton.click(function () {
   let searchInput = searchInputElement.val();
   recentSearches.push(searchInput);
   localStorage.setItem('search', JSON.stringify(recentSearches));
   recentSearch();
+  weatherResults(searchInput);
 
-  var apiKey = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&units=imperial&appid=57b85d9f6f550dd234e00f49ef09169c`;
-  //console.log("This is the input", searchInput)
-  $.ajax({
-    url: apiKey,
-    method: "GET",
-    success: function (data) {
-      var cityTitle = $("#cityName");
-      var temp = $("#temp");
-      var humid = $("#humidity");
-      var wind = $("#windSpeed");
-      var dateString = moment.unix(data.dt).format("MM/DD/YYYY");
-      cityTitle.html(
-        `${data.name} ${dateString} <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>`
-      );
-      temp.text(data.main.temp);
-      humid.text(data.main.humidity);
-      wind.text(data.wind.speed);
-      //console.log(data)
-      var oneCallApiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=57b85d9f6f550dd234e00f49ef09169c`;
-      //console.log(data.coord.lat)
-      //console.log(data.coord.lon)
-      $.ajax({
-        url: oneCallApiKey,
-        method: "GET",
-        success: function (data) {
-          console.log("This is the One Call API Key!", data);
-          console.log("This is the UV Index", data.current.uvi);
-          var uvElement = $("#uvInd");
-          if (data.current.uvi <= 2) {
-            uvElement.css("background-color", "green");
-          } else if (data.current.uvi >= 3 && data.current.uvi <= 5) {
-            uvElement.css("background-color", "yellow");
-          } else if (data.current.uvi >= 6 && data.current.uvi <= 7) {
-            uvElement.css("background-color", "orange");
-          } else {
-            uvElement.css("background-color", "red");
-          }
-          uvElement.text(data.current.uvi);
-          //5 day forecast
-          var fiveDay = $("#fivedayforecast");
-          fiveDay.empty();
-          //fiveDay.text(data.daily[1].humidity)
-          //console.log("this is the daily humidity for day 1", data.daily[1].humidity)
-
-          for (var i = 1; i < 6; i++) {
-            //fiveDay.text(data.daily[i].humidity)
-            console.log(data.daily[i]);
-            var divElement = $("<div class=fivedaybox></div>");
-            var dateElement = $("<p></p>");
-            var datefive = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
-            dateElement.text(datefive);
-            var iconElement = $(
-              `<img src="http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"/>`
-            );
-            var tempElement = $("<p></p>");
-            var tempfive = data.daily[i].temp.day;
-            tempElement.text(tempfive);
-            var humidityElement = $("<p></p>");
-            humidityElement.text(data.daily[i].humidity);
-            divElement.append(
-              dateElement,
-              iconElement,
-              tempElement,
-              humidityElement
-            );
-            fiveDay.append(divElement);
-
-            //tempterature, humidity, date, icon
-          }
-        },
-      });
-    },
-  });
+  //New function here to get data from search
+  //function weatherResults() {}
+  
 
 });
 
 function recentSearch() {
     $('#searchList').empty();
     for (let i = 0; i < recentSearches.length; i++) {
-        const listElement = $("<li>");
+        const listElement = $("<li class='listSearch'>");
         listElement.text(recentSearches[i]);
         $("#searchList").append(listElement);
     }
-    $('#searchList').click(function() {
-        alert("clicked it");
-        //I want the word clicked to go into the searchInput and run through the function again
-
-    });
-
 }
+
+$('.listSearch').click(function() {
+    console.log($(this).text());
+    weatherResults($(this).text());
+
+    //I want the word clicked to go into the searchInput and run through the function again
+
+});
+
+//To Do Now
+//Separate code that handles the search and displaying the data into its own function
+//One function take city name and then call the api and get data and update html
+//Need two click handlers
+//One click is for the button that starts a new search (get city name from search input and send to search function which will search and update html)
+//another function for old searched that are clicked
+//use event.target to get a reference to what was clicked and get city name and then call search with city name
 
 
 
